d57177c1abd8083db66b95a69f523a9af0465892
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-05-09 15:54:06 -0300

    commit

---
+++ Modulo_1/Imports e Exports parte 1/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let n1 = 10;
+let n2 = 2;
+console.log('Soma: ');
+console.log('Subtração: ');
+console.log('Multiplicação: ');
---
+++ Modulo_1/Imports e Exports parte 1/src/index.ts
@@ -0,0 +1,9 @@
+const math = require('./math.ts')
+
+let n1: number = 10
+let n2: number = 2
+
+console.log(`Soma: ${math.somar(n1, n2)}`)
+console.log(`Subtrair: ${math.subtrair(n1, n2)}`)
+console.log(`multiplicar: ${math.multiplicar(n1, n2)}`)
+
