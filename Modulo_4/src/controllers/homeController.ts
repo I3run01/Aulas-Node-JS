5dc04dfcfd3e393c6048a483ef76250550df2c67
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-06-08 09:43:26 -0300

    commit

--- Modulo_4/src/controllers/homeController.ts
+++ Modulo_4/src/controllers/homeController.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
 
 import { Product } from '../models/Product';
+import  User  from '../models/User'
 
-export const home = (req: Request, res: Response)=>{
+export const home = async (req: Request, res: Response)=>{
+    let usuarios = await User.find({})
+    console.log("Usuários", usuarios)
+
     let age: number = 90;
     let showOld: boolean = false;
 
