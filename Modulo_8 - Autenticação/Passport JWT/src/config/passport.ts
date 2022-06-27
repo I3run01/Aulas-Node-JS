dd2e7e43699f8cf01494b37adc1ff6c94bb0b107
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-06-27 15:42:44 -0300

    commit

--- Modulo_8 - Autenticação/Passport JWT/src/config/passport.ts
+++ Modulo_8 - Autenticação/Passport JWT/src/config/passport.ts
@@ -1,27 +1,29 @@
-import { Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction }from  'express'
 import passport from "passport";
-import { BasicStrategy } from "passport-http";
-import { User } from "../models/User";
+import dotenv from 'dotenv'
+import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
+import { User} from '../models/User'
+
+dotenv.config()
 
 const notAuthorizedJson = {status: 401, message: 'Não autorizado'}
+const options = {
+    jwtFromRequest:  ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET as string
+}
 
-passport.use(new BasicStrategy(async (email, password, done) => {
-    if(email && password) {
-        const user = await User.findOne({
-           where: { email, password} 
-        })
-        if(user) {
-            return done(null, user)
-        }
-    }
-    return done(notAuthorizedJson, false)
+passport.use(new JWTStrategy(options, async (payload, done) => {
+    const user = await User.findByPk(payload.id)
+    if(user) done(null, user)
+    else done(notAuthorizedJson, false)
 }))
 
-export const privateRoute = (req: Request, res: Response, next: NextFunction) => {
-    const authFunction = passport.authenticate('basic', (err, user) => {
+export const privateRoute = (req: Request, res: Response, next: NewableFunction) => {
+    passport.authenticate('jwt', (err, user) => {
         req.user = user
-        return user ? next() : next(notAuthorizedJson)
-    })(req, res, next)
+        return user ? next() : next(notAuthorizedJson)}
+        )(req, res, next())
+    
 }
 
 export default passport
