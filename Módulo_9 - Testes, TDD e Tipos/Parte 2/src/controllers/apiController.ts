1d488e39a144019b3e810b756c78626053a036a6
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-06-29 15:58:52 -0300

    commit

--- Módulo_9 - Testes, TDD e Tipos/Parte 2/src/controllers/apiController.ts
+++ Módulo_9 - Testes, TDD e Tipos/Parte 2/src/controllers/apiController.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from 'express';
-import { User } from '../models/User';
+import e, { Request, Response } from 'express';
+import { rmSync } from 'fs';
+import * as UserService from '../services/UserServices'
 
 export const ping = (req: Request, res: Response) => {
     res.json({pong: true});
@@ -9,18 +10,17 @@ export const register = async (req: Request, res: Response) => {
     if(req.body.email && req.body.password) {
         let { email, password } = req.body;
 
-        let hasUser = await User.findOne({where: { email }});
-        if(!hasUser) {
-            let newUser = await User.create({ email, password });
+        const newUser = await UserService.createUser(email, password)
 
-            res.status(201);
-            res.json({ id: newUser.id });
+        if(newUser instanceof Error) {
+            res.json({error: newUser.message})
         } else {
-            res.json({ error: 'E-mail já existe.' });
+            res.status(201)
+            res.json( { id: newUser.id})
         }
     }
 
-    res.json({ error: 'E-mail e/ou senha não enviados.' });
+    else res.json({ error: 'E-mail e/ou senha não enviados.' });
 }
 
 export const login = async (req: Request, res: Response) => {
@@ -28,21 +28,18 @@ export const login = async (req: Request, res: Response) => {
         let email: string = req.body.email;
         let password: string = req.body.password;
 
-        let user = await User.findOne({ 
-            where: { email, password }
-        });
+        const user = await UserService.findByEmail(email)
 
-        if(user) {
+        if (user && UserService.matchPassword(password, user.password)) {
             res.json({ status: true });
-            return;
         }
     }
 
-    res.json({ status: false });
+    else res.json({ status: false });
 }
 
 export const list = async (req: Request, res: Response) => {
-    let users = await User.findAll();
+    let users = await UserService.all()
     let list: string[] = [];
 
     for(let i in users) {
